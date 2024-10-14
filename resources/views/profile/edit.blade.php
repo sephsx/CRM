98405bead7e57914b5a6f23c2c661883f6673cae
sephsx <olorbidajoseph05@gmail.com>
2024-10-14 21:29:33 +0800

    ...

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,7 +5,6 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
             <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
@@ -19,7 +18,6 @@
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
-
             <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
